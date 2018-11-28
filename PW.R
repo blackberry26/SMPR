6fb2b44b4edba674734755103482b371a48dc393
blackberry26 <43415122+blackberry26@users.noreply.github.com>
2018-11-28 17:17:30 +0300

    Update PW.R

--- PW.R
+++ PW.R
@@ -2,46 +2,46 @@ euclideanDistance <- function(u, v)
 {
 return(sqrt(sum((u - v)^2)))
 }
-kernelEP = function(r){
+coreEP = function(r){
   return ((3/4*(1-r^2)*(abs(r)<=1)))
 }
-kernelR = function(r){
+coreR = function(r){
   return ((0.5 * (abs(r) <= 1) ))
 } 
-kernelT = function(r){
+coreT = function(r){
   return ((1 - abs(r)) * (abs(r) <= 1)) 
 }
-kernelQ = function(r){
+coreQ = function(r){
   return ((15 / 16) * (1 - r ^ 2) ^ 2 * (abs(r) <= 1)) 
 }
-kernelG = function(r){
+coreG = function(r){
   return (((2*pi)^(-1/2)) * exp(-1/2*r^2))
 }
 
 
 
-PW= function(XL,y,h,metricFunction = euclideanDistance)
+PW= function(XL,y,h,metricFunction = euclideanDistance) # Оценка весовой функции по расстояниюу 
 {
   n = dim(xl)[1]
   weights = rep(0,3)
   names(weights) = c("setosa", "versicolor", "virginica")
-  for(i in 1:n)
+  for(i in 1:n) # Для всех объектов выборки
   {
     
     x=XL[i,1:2]
-    class=XL[i,3]
+    class=XL[i,3] # Берём его класс
     
     r = metricFunction(x,y)/h
-    weights[class]=kernelG(r)+weights[class];
+    weights[class]=coreG(r)+weights[class]; # И прибавляем его вес к общему весу его класса
   }
   
   class = names(which.max(weights))
   
-  if(max(weights)==0){
-    return ("No")
+  if(max(weights)==0){ # Если веса точки по классам равны 0 (точка не попала в окно)
+    return (0) #вернуть 0
   }
-  else{
-    return (class)
+  else{ # иначе
+    return (class) #  Вернуть класс с максимальным весом
   }
 }
 
@@ -49,8 +49,8 @@ plotWindows = function(h){
   for(i in seq(0, 7, 0.1)){
     for(j in seq(0,3,0.1)){
       z = c(i, j)
-      class = Parzen(xl,z,h)
-      if(class!="No"){
+      class = PW(xl,z,h)
+      if(class!=0){
         points(z[1], z[2], pch = 1,col=colors[class])
       }
     }
@@ -78,18 +78,18 @@ LOO = function(xl,class) {
   
   loo = loo / n
   x = seq(0.1,2,0.1)
-  plot(x, loo,main ="LOO for PW(H)", xlab="h", ylab="LOO", type = "l")
+  plot(x, loo,main ="LOO fдля PW(H)", xlab="h", ylab="LOO", type = "l")
   
   min=which.min(loo)
   lOOmin=round(loo[min],3)
   minX=min/10
   
-  points(minX, loo[min], pch = 21, col = "red",bg = "red")
+  points(minX, loo[min], pch = 21, col = "red",bg = "red"
   label = paste("   H = ", minX, "\n", "   LOO = ", lOOmin, sep = "")
   text(minX, lOOmin, labels = label, pos=4, col = "red")
   
   
-  text = paste("Map classificaton for PW(Gaussian) with h = ", minX)
+  text = paste("Кaрта классификации для PW(GS)")
   plot(iris[, 3:4],main=text, pch = 21, bg = colors[xl$Species], col = colors[xl$Species],asp='1')
   plotWindows(minX)
 }
